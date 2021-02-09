57e67bc5b6a73a05ce2450bb16119b9ce01ef236
Joshua Burger <joshberger6969@outlook.com>
2021-02-08 21:18:43 -0500

    new migration table

--- db/migrate/20210209020106_create_intakes.rb
+++ db/migrate/20210209020106_create_intakes.rb
@@ -1,4 +1,10 @@
 class CreateIntakes < ActiveRecord::Migration[5.2]
   def change
+    create_table :intakes do |t|
+      t.string :name
+      t.integer :amount
+      t.integer :user_id
+      t.integer :days_id
+    end
   end
 end
