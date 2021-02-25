9df6c86cd8e7d8b723e345352ffde47474b2b2ab
Joshua Burger <joshberger6969@outlook.com>
2021-02-24 21:10:40 -0500

    intake_controller.rb

--- app/controllers/intake_controller.rb
+++ app/controllers/intake_controller.rb
@@ -2,45 +2,86 @@ class IntakeController < ApplicationController
 
     #create
     get '/intakes/new' do
-        erb :'/intakes/new'
+        if current_user = logged_in?
+            erb :'/intakes/new'
+        else
+            erb :'/users/failure'
+        end
     end
 
     post '/intakes' do
-        @intake = Intake.create(name: params[:name], amount: params[:amount])
-        redirect "/intakes/#{@intake.id}"
+        current_user
+        @intake = current_user.intakes.create(name: params[:name], amount: params[:amount], date: params[:date])
+        if @intake.save
+            redirect "/users/#{current_user.id}"
+        else
+            redirect '/intakes/new'
+        end
     end
 
     #read
 
     get '/intakes/:id' do
+        current_user
         @intake = Intake.find(params[:id])
-        erb :'/intakes/show'
+        if @intake.save
+            erb :'/intakes/show'
+        else
+            erb :'/intakes'
+        end
     end
 
     get '/intakes' do
-        @intakes = Intake.all #returns an array
-        erb :'/intakes/index'
+        if current_user = logged_in?
+            @intakes = Intake.all #returns an array
+            erb :'/intakes/index'
+        else
+            erb :'/users/failure'
+        end
     end
 
     #update
 
     get '/intakes/:id/edit' do
-        @intake = Intake.find(params[:id])
-        erb :'/intakes/edit'
+        if logged_in?
+            @intake = Intake.find(params[:id])
+            erb :'/intakes/edit'
+        else
+            redirect '/home'
+        end
     end
 
-    post '/intakes/:id' do
+    patch '/intakes/:id' do
+        current_user
         @intake = Intake.find(params[:id])
-        @intake.update(name: params[:name], amount: params[:amount])
-        redirect "/intakes/#{@intake.id}"
+        if logged_in? && @intake.user_id == current_user.id
+            @intake.update(name: params[:name], amount: params[:amount], date: params[:date])
+            redirect "/users/#{current_user.id}"
+        else
+            erb :'/users/failure2'
+        end
+        # if logged_in? && @intake.id == current_user.id
+        #     @intake.name = params[:name]
+        #     @intake.amount = params[:amount]
+        #     @intake.date = params[:date]
+        #     @intake.save
+        #     redirect "/intakes"
+        # else
+        #     redirect '/intakes'
+        # end
     end
 
     #delete
 
     delete '/intakes/:id' do
+        current_user
         @intake = Intake.find(params[:id])
-        @intake.destroy
-        redirect '/intakes'
+        if logged_in? && @intake.user_id == current_user.id
+            @intake.destroy
+            redirect "/users/#{current_user.id}"
+        else
+            erb :'/users/failure2'
+        end
     end
 
 end
