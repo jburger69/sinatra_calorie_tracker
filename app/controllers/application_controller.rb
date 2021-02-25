3d48b20fbd27c52e375bade6d9dcafaa46c71463
Joshua Burger <joshberger6969@outlook.com>
2021-02-24 21:09:55 -0500

    application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,26 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    session.clear
     erb :welcome
+  end
+
+  get "/home" do
+    if logged_in? == true
+      redirect "/users/#{current_user.id}"
+    else
+      erb :welcome
+    end
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
